c6b62da0a94c92ffae13cac0278c9400d234a5b8
Pall2006 <pall2006@bk.ru>
2024-05-07 12:12:20 +0300

    8. Date. Отображение даты. [#310017 #494537]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
-        Item it = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(it.getCreate().format(formatter));
+//        Item it = new Item();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+//        System.out.println(it.getCreated().format(formatter));
     }
 }
