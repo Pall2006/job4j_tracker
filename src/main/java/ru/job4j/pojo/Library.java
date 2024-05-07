62126d3b464d571b6bb6eb5b13b264f50f3fc864
Pall2006 <pall2006@bk.ru>
2024-05-07 11:54:05 +0300

    3. Массивы и модели. [#174892 #494792]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -25,7 +25,7 @@ public class Library {
                     + " Количестов штук - " + book.getCount());
         }
         for (int index = 0; index < books.length; index++) {
-            if (books[index].getName().equals("Clean code")) {
+            if ("Clean code".equals(books[index].getName())) {
                 System.out.println(books[index].getName());
             }
         }
