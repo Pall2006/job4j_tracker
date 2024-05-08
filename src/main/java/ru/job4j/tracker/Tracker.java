fc416d0b866d511b4689814d6aecd491bd9b3776
Pall2006 <pall2006@bk.ru>
2024-05-08 17:36:44 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,4 +55,15 @@ public class Tracker {
         }
         return false;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = null;
+            System.arraycopy(items, index + 1,
+                    items, index,
+                    size - 1 - index);
+            size--;
+        }
+    }
 }
