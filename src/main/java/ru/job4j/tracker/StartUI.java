25a163fd9c91720b1e9d1c84819cee50636ed3dd
Pall2006 <pall2006@bk.ru>
2024-05-09 17:29:02 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,8 +3,7 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
-        Item it = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(it.getCreated().format(formatter));
+        Item it = new Item(5, "Подготовить отчет");
+        System.out.println(it);
     }
 }
