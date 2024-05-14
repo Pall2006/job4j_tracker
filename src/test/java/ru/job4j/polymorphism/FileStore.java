b68765b6cc074a2977b31fcc6e554abd7f5536d9
Pall2006 <pall2006@bk.ru>
2024-05-14 19:46:21 +0300

    2.1. Scanner и чтение числа из консоли. [#179518] -222

--- src/test/java/ru/job4j/polymorphism/FileStore.java
+++ src/test/java/ru/job4j/polymorphism/FileStore.java
@@ -3,18 +3,17 @@ package ru.job4j.polymorphism;
 public class FileStore {
     private String path;
 
-    public FileStore() { }
+    public FileStore() {
+    }
 
     public FileStore(String path) {
         this.path = path;
     }
 
     public void save(String data) {
-        //Тут реализация хранения данных в файле
     }
 
     public String read() {
-        // Тут чтение файла
         return null;
     }
 }
