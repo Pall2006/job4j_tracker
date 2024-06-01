0ee1dbeb673af366bf3073c3c58fce18c04b6546
Pall2006 <pall2006@bk.ru>
2024-06-01 14:19:45 +0300

    тест2

--- src/main/java/ru/job4j/lesson23/Test2.java
+++ src/main/java/ru/job4j/lesson23/Test2.java
@@ -2,27 +2,31 @@ package ru.job4j.lesson23;
 
 public class Test2 {
     public static void main(String[] args) {
-        Doctor d = new Doctor();
-        Employee dy = new Doctor();
-        System.out.println(dy.name);
-        Employee xr = new Xirurg();
+        Doctor doctor = new Doctor();
+        Employee empDoctor = new Doctor();
+        Employee empXr = new Xirurg();
         Xirurg xirurg = new Xirurg();
-        Doctor xirurg2 = new Xirurg();
-        xr.eat();
-        xirurg2.sleep();
-        System.out.println(xr.name);
+        Employee empXirurg = new Xirurg();
+        Employee empDriver = new Driver();
+        Teacher teacher = new Teacher();
+        Test2 test2 = new Test2();
+        System.out.println(doctor.name);
+        System.out.println(empDoctor.name);
     }
 }
 
-class Employee {
+interface HelpAble {
+    void help();
+}
 
+class Employee {
     double salary = 1000;
     String name = "Kolya";
     int age;
     int experience;
 
-    void eat() {
-        System.out.println("Kushaet");
+    static void eat() {
+        System.out.println("Kushaet employee");
     }
 
     void sleep() {
@@ -30,24 +34,53 @@ class Employee {
     }
 }
 
-class Doctor extends Employee {
+class Doctor extends Employee implements HelpAble {
 
     String name = "Jora";
-    
+    double salary = 1000;
+
     void lechit() {
         System.out.println("Lechit");
     }
 
-    void eat() {
+    static void eat() {
         System.out.println("Kushaet Doctor");
+    }
+
+    @Override
+    public void help() {
+        System.out.println("The doctor treats");
     }
 }
 
 class Xirurg extends Doctor {
 
-    String nameCar;
+    String couuntOfPation;
 
-    void eat() {
+    void operiruet() {
+        System.out.println("The surgeon performs operations");
+    }
+
+    static void eat() {
         System.out.println("Kushaet Xirurg");
+    }
+}
+
+class Driver extends Employee {
+    String nameOfCar = "Lada";
+
+    void drive() {
+        System.out.println("Водитель водит");
+    }
+}
+
+class Teacher extends Employee implements HelpAble {
+    void teach() {
+        System.out.println("The teacher teaches children");
+    }
+
+    @Override
+    public void help() {
+        System.out.println("The teacher helps helps the students");
     }
 }
