a6d4a4e5c8abe84e2db92b221354d37e47b36079
Pall2006 <pall2006@bk.ru>
2024-08-15 11:10:44 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
+
         ArrayList<String> names = new ArrayList<>();
         names.add("Petr");
         names.add("Ivan");
