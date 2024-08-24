631f7441a1b5175c98b535d92d197d21b899f771
Pall2006 <pall2006@bk.ru>
2024-08-24 11:19:38 +0300

    Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    @SuppressWarnings("checkstyle:EmptyBlock")
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (tasks.isEmpty()) {
+                tasks.add(index, element);
+                break;
+            } else if (element.getPriority() == task.getPriority()) {
+                index++;
+                continue;
+            } else if (element.getPriority() < task.getPriority()) {
+                index = task.getPriority() - 1;
+                break;
+            }
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
---
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriorityQueueTest {
+    @Test
+    public void whenHigherPrioritySecond() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDescription()).isEqualTo("urgent");
+    }
+
+    @Test
+    public void whenHigherPriorityEquals() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 5));
+        queue.put(new Task("middle", 5));
+        Task result = queue.take();
+        assertThat(result.getDescription()).isEqualTo("low");
+    }
+}
