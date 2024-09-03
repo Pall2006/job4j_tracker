9af0bb66bc4790b2f554253c9325f58aeb790ffc
Pall2006 <pall2006@bk.ru>
2024-09-03 11:05:22 +0300

    5. Очередь с приоритетом на LinkedList [#41670 #510393]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -5,12 +5,10 @@ import java.util.LinkedList;
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
-    @SuppressWarnings("checkstyle:EmptyBlock")
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
             if (element.getPriority() > task.getPriority()) {
-                index = task.getPriority() - 1;
                 break;
             }
             index++;
